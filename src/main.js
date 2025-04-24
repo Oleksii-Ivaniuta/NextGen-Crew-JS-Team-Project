1d18687579fb24e2b5b9f872868f6771db68a651
Oleksii Ivaniuta <11.ivaniuta@gmail.com>
2025-04-24 11:56:45 +0100

    add js files, add fonts

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './js/theme.js'
+import './js/mobile-menu';
+import './js/tech-skills';
+import './js/my-projects';
+import './js/faq';
+import './js/reviews';
+import './js/work-together';
+import './js/modal-window';
